bd526330d266883af051ca1b8c34e60c4722db09
Innocent <innocent.amadi@andela.com>
2016-02-20 21:36:34 +0100

    removes absolute paths from image assets

--- js/game_properties.js
+++ js/game_properties.js
@@ -13,17 +13,17 @@ var gameStates = {
 }
 
 var assets = {
-  entryBackground: {name: "entryBackground", url: "/assets/sky.png"},
-  gameBackground: {name: "gameBackground", url: "/assets/field.jpg"},
-  pauseBackground: {name: "pauseBackground", url: "/assets/green_locks.png"},
-  startButton: {name: "startButton", url: "/assets/start_button.png"},
-  pauseButton: {name: "pauseButton", url: "/assets/pause_button.png"},
-  playButton: {name: "playButton", url: "/assets/play_button.png"},
-  quitButton: {name: "quitButton", url: "/assets/quit_button.png"},
-  player: {name: "player", url: "/assets/player.png"},
-  pauseText: {name: "pauseText", url: "/assets/pause_text.png"},
-  gameLevelOne: {name: "level1", url: "/assets/tilemaps/level1.json"},
-  tiles: {name: "gameTiles", url: "/assets/tiles.png" },
-  star: {name:"star", url: "/assets/star.png"},
-  questionMark: {name: "question_mark", url: "/assets/question_mark.png"}
+  entryBackground: {name: "entryBackground", url: "assets/sky.png"},
+  gameBackground: {name: "gameBackground", url: "assets/field.jpg"},
+  pauseBackground: {name: "pauseBackground", url: "assets/green_locks.png"},
+  startButton: {name: "startButton", url: "assets/start_button.png"},
+  pauseButton: {name: "pauseButton", url: "assets/pause_button.png"},
+  playButton: {name: "playButton", url: "assets/play_button.png"},
+  quitButton: {name: "quitButton", url: "assets/quit_button.png"},
+  player: {name: "player", url: "assets/player.png"},
+  pauseText: {name: "pauseText", url: "assets/pause_text.png"},
+  gameLevelOne: {name: "level1", url: "assets/tilemaps/level1.json"},
+  tiles: {name: "gameTiles", url: "assets/tiles.png" },
+  star: {name:"star", url: "assets/star.png"},
+  questionMark: {name: "question_mark", url: "assets/question_mark.png"}
 }
